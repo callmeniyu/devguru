b7a01fcb4066599d1c4f9a16a3d7426e22dae727
callmeniyu <getniyashere@gmail.com>
2024-09-08 10:30:21 +0530

    add muhsin to thteam

--- client/src/components/Team/Team.jsx
+++ client/src/components/Team/Team.jsx
@@ -7,6 +7,7 @@ import niyas_img from "../../assets/images/me.jpg"
 import shihad_img from "../../assets/images/shihad_img.jpeg"
 import rizad_img from "../../assets/images/rizad_img.jpeg"
 import sadiq_img from "../../assets/images/sadiq_img.jpg"
+import muhsin_img from "../../assets/images/muhsin_img.jpg"
 const Team = () => {
     return (
         <div className="team">
@@ -90,6 +91,19 @@ const Team = () => {
                             github: "https://github.com/4TechSadiq",
                             linkedin: "https://www.linkedin.com/in/moh-sadiq-ali/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
                             img: sadiq_img,
+                        }}
+                    />
+                </SplideSlide>
+                <SplideSlide key={5} className="splide-container">
+                    <IdCard
+                        data={{
+                            name: "Muhsin Kalodi",
+                            stack: "Frontend Engineer",
+                            experience: "2",
+                            projects: "10",
+                            github: "https://github.com/muhsinkalodi",
+                            linkedin: "https://www.linkedin.com/in/muhsin-kalodi-8872531b5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+                            img: muhsin_img,
                         }}
                     />
                 </SplideSlide>
